9f439ae2f25c7a1d3665ca4f174e3a80cfbbdbc8
unknown <justiiturraspe@gmail.com>
2022-03-23 10:54:14 -0300

    css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useEffect, useState } from 'react';
 import { db } from './firebase.js';
 import Painting from './Painting';
+import Header from './Header';
 
 <script src="https://kit.fontawesome.com/cbff21ecde.js" crossorigin="anonymous"></script>
 
@@ -21,21 +21,8 @@ function App() {
 
   return (
     <div className="app">
-      <header className="header">
-
-        <div className="leftHeader">
-          <img src="https://cdn-icons-png.flaticon.com/512/2972/2972163.png" className="App-logo" alt="logo" width="80"/>
-          <h1>Jorgelina Parkinson</h1>
-        </div>
-
-        <div className="rightHeader">
-          <h3>Home</h3>
-          <h3>Series</h3>
-          <h3>Awards</h3>
-          <h3>About</h3>
-        </div>
-
-      </header>
+      
+      <Header />
 
       <div className='gallery'>
         {
